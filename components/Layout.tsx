2ecb5febeb85f5585ef3a0bf38201c9b8700ccb3
EdwardLH219 <elownehughes@gmail.com>
2025-04-02 18:17:17 +0200

    Move font stylesheet to _document.tsx for better Next.js practices

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -134,7 +134,6 @@ export default function Layout({ children }: LayoutProps) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </Head>
       
       <header className="sticky-header bg-white/90 backdrop-blur py-6">
